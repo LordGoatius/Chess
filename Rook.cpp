6c11ff990ee30b360e5aaa988a9170550a920cc5
LordGoatius <jtostler1@gmail.com>
2023-03-01 13:57:49 -0800

    Rook Move function updated

--- Rook.cpp
+++ Rook.cpp
@@ -8,6 +8,13 @@ private:
     bool color;
 public:
     Rook(void) : x(0), y(0), color(false) {}
+
+    bool move(unsigned new_x, unsigned new_y) {
+        if ((new_x < 8 && new_y == y) || (new_y < 8 && new_x == x)) {
+            return true;
+        }
+        return false;
+    }
 };
 
 #endif
